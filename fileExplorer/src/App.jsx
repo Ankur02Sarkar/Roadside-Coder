34cba7535ee431bfa16bbd677cbfa290bcc2791f
Ankur Sarkar <sarkarrohonankur@gmail.com>
2024-02-29 09:09:59 +0530

    update

--- fileExplorer/src/App.jsx
+++ fileExplorer/src/App.jsx
@@ -4,15 +4,46 @@ import explorer from "./data";
 
 const Folder = ({ folderItem, level = 0 }) => {
   const [show, setShow] = useState(false);
+  const [showForm, setShowForm] = useState({
+    visible: false,
+    isFolder: null
+  });
 
   const handleClick = (e) => {
-    e.stopPropagation(); // important to avoid rerender of parent folder
+    e.stopPropagation();
     setShow(!show);
+  };
+  const addFolder = (e, parentItem) => {
+    e.stopPropagation();
+    setShowForm({
+      visible: true,
+      isFolder: true
+    });
+  };
+  const addFile = (e, parentItem) => {
+    e.stopPropagation();
+    setShowForm({
+      visible: true,
+      isFolder: false
+    });
   };
 
   return (
     <div onClick={handleClick}>
-      <div style={{ cursor: "pointer" }}>📁 {folderItem.name}</div>
+      <div style={{ cursor: "pointer", display: "flex", gap: "1rem" }}>
+        <span>📁 {folderItem.name}</span>
+        <div>
+          <button onClick={(e) => addFolder(e)}>+ Folder</button>
+          <button onClick={(e) => addFile(e)}>+ File</button>
+        </div>
+      </div>
+      {showForm.visible &&
+        <div>
+          {showForm.isFolder ? "📁" : "📄"}
+          <input type="text" placeholder={showForm.isFolder ? "Enter Folder Name" : "Enter File Name"} />
+          <button>Save</button>
+        </div>
+      }
       {show && (
         <div style={{ marginLeft: `${level * 1.25}rem` }}>
           {folderItem.items.map((item, index) => (
